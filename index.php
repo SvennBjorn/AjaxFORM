1c7c2336a6ca17687460afc7bc25d92f6c24c060
Stevan Guillermet <sbg@hotmail.fr>
2018-05-07 10:13:08 +0200

    _Exo formulaire Ajax + insertion BDD (1)

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+define("PATHCONF", "./conf/");
+date_default_timezone_set("Europe/Paris");
+require_once "./functions/classAutoLoader.php";
+spl_autoload_register('classAutoLoader');
+
+$test = new Form(PATHCONF, "registration");
+
+echo $test->frmGenerate("form.php");
+
+
